5613044378a34f9c399a89648fa18487b8383792
Volodymyr-bart <61430064+Volodymyr-bart@users.noreply.github.com>
2024-03-07 13:38:50 +0200

    change url

--- src/common/common.ts
+++ src/common/common.ts
@@ -1,2 +1,2 @@
-// export const URL_SERVER = "https://eliftech-backend-xg1n.onrender.com";
-export const URL_SERVER = "http://localhost:5000";
+export const URL_SERVER = "https://eliftech-backend-xg1n.onrender.com";
+// export const URL_SERVER = "http://localhost:5000";
